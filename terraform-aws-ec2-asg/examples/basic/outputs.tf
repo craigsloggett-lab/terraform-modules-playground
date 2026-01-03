5c48041aef87ddd74a070814a632554e8b8cc5de
Craig Sloggett <craigsloggett@gmail.com>
2026-01-03 12:19:56 -0500

    Add module scaffolding for EC2 ASG

--- terraform-aws-ec2-asg/examples/basic/outputs.tf
+++ terraform-aws-ec2-asg/examples/basic/outputs.tf
@@ -1,14 +1,14 @@
 output "aws_caller_identity" {
   description = "The AWS caller identity data source."
-  value       = module.terraform_aws_alb.aws_caller_identity
+  value       = module.terraform_aws_ec2_asg.aws_caller_identity
 }
 
 output "aws_region" {
   description = "The AWS region (current) data source."
-  value       = module.terraform_aws_alb.aws_region
+  value       = module.terraform_aws_ec2_asg.aws_region
 }
 
 output "aws_availability_zones" {
   description = "The AWS availability zones (available) data source."
-  value       = module.terraform_aws_alb.aws_availability_zones
+  value       = module.terraform_aws_ec2_asg.aws_availability_zones
 }
